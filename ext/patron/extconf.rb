313e9ae6e8b19840dc55509a918808cc1de440ca
Mehdi <mehdi.adda@gmail.com>
2011-03-09 19:15:25 -0800

    make it works on windows with curl installed

--- ext/patron/extconf.rb
+++ ext/patron/extconf.rb
@@ -29,9 +29,20 @@ end
 require 'mkmf'
 require 'rbconfig'
 
+dir_config('curl')
+
 if find_executable('curl-config')
-  $CFLAGS << " #{`curl-config --cflags`.strip}"
-  $LIBS << " #{`curl-config --libs`.strip}"
+  $CFLAGS << " #{`curl-config --cflags`.strip} -g"
+  if ENV['STATIC_BUILD']
+    $LIBS << " #{`curl-config --static-libs`.strip}"
+  else
+    $LIBS << " #{`curl-config --libs`.strip}"
+  end
+  ca_bundle_path=`curl-config --ca`.strip
+  if !ca_bundle_path.nil? and ca_bundle_path != ''
+    $defs.push( %{-D HAVE_CURL_CONFIG_CA} )
+    $defs.push( %{-D CURL_CONFIG_CA='#{ca_bundle_path.inspect}'} )
+  end
 elsif !have_library('curl') or !have_header('curl/curl.h')
   fail <<-EOM
   Can't find libcurl or curl/curl.h
